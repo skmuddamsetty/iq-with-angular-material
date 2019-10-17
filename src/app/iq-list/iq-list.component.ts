eef540edb3d2ebc10631cd333ff94f7c3420c83e
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-10-16 22:46:27 -0400

    added styling for iq side nav list

--- src/app/iq-list/iq-list.component.ts
+++ src/app/iq-list/iq-list.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iq-list.component.scss']
 })
 export class IQListComponent implements OnInit {
-  showFiller = false;
+  iqArray = [
+    {
+      question: 'This is First Question',
+      answer: 'This is First Answer'
+    },
+    {
+      question: 'This is Second Question',
+      answer: 'This is Second Answer'
+    }
+  ];
   constructor() {}
 
   ngOnInit() {}
